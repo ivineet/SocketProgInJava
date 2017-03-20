30434e5ad6f1ac9dab2f7ebe12528b7661c22b69
koder <Vineet@Vineet-PC>
2017-03-20 23:46:22 +0530

    Socket programming

--- SocketProgInJava/src/Client.java
+++ SocketProgInJava/src/Client.java
@@ -1,8 +1,11 @@
-
+/*
+ * http://quiz.geeksforgeeks.org/socket-programming-in-java/
+ * www.geeksforgeeks.org/simple-calculator-using-tcp-java/
+ */
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
